fa47142966012343e1aa63a07d969e060f562f71
andynvt <0188andY@>
2018-10-28 11:33:46 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -149,3 +149,7 @@ Route::get('yeu-thich',[
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
 
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
